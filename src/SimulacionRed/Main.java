628da71981f4e471302ede31e1843985c68dd7f7
JavierSalazarG <javizz.hjsg@gmail.com>
2017-08-03 18:20:00 -0700

    sync

--- src/SimulacionRed/Main.java
+++ src/SimulacionRed/Main.java
@@ -3,7 +3,7 @@ package SimulacionRed;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO: 28/07/17 modelar clases para hardware (terminales, routers, switch, etc), IPs, MACs, agrupar por rangos de IP, IP tables, Routing, Forwarding
+        // TODO: 28/07/17 modelar clases para hardware, IPs, MACs, agrupar por rangos de IP, IP tables, Routing, Forwarding
         // TODO: 28/07/17 Revisar conforme se avanze que getters y setters si se van a ocupan y eliminar el resto
 
     }
